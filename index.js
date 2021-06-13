8308a701340354d8781fe582c47bc6ebd7738289
jsato <jsato@yahoo-corp.jp>
2021-06-13 23:04:10 +0900

    3.9 wip

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
+const cors = require('cors')
 const express = require('express')
 const morgan = require('morgan')
 
 const app = express()
 
+app.use(cors())
 app.use(express.json())
 
 morgan.token('body', (req, res) => {
